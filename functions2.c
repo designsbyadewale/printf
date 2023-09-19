d830c2fee961d265d52ace8c6bdcfbde6792854c
designsbyadewale <designsbyadewale@gmail.com>
2023-09-19 16:13:38 -0700

    update

--- functions2.c
+++ functions2.c
@@ -1,19 +1,26 @@
 #include "main.h"
 
+/* Function Declarations */
+int write_pointer(char buffer[], int ind, int length, int width,
+		int flags, char padd, char extra_c, int padd_start);
+int is_printable(char c);
+int append_hexa_code(char c, char buffer[], int index);
+
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
- * @types: List a of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * @flags: Calculates active flags
+ * @width: Get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed.
+ * Return: Number of characters printed
  */
 int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
+	/* Initialize variables */
 	char extra_c = 0, padd = ' ';
 	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
 	unsigned long num_addrs;
@@ -23,14 +30,17 @@ int print_pointer(va_list types, char buffer[],
 	UNUSED(width);
 	UNUSED(size);
 
+	/* Check if the pointer is NULL */
 	if (addrs == NULL)
 		return (write(1, "(nil)", 5));
 
+	/* Set the last character of buffer to null */
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
 
 	num_addrs = (unsigned long)addrs;
 
+	/* Convert the pointer value to hexadecimal */
 	while (num_addrs > 0)
 	{
 		buffer[ind--] = map_to[num_addrs % 16];
@@ -38,6 +48,7 @@ int print_pointer(va_list types, char buffer[],
 		length++;
 	}
 
+	/* Handle flags for padding and extra characters */
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
@@ -47,24 +58,23 @@ int print_pointer(va_list types, char buffer[],
 
 	ind++;
 
-	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
-	return (write_pointer(buffer, ind, length,
-		width, flags, padd, extra_c, padd_start));
+	return (write_pointer(buffer, ind, length, width,
+				flags, padd, extra_c, padd_start));
 }
 
 /************************* PRINT NON PRINTABLE *************************/
 /**
- * print_non_printable - Prints ascii codes in hexa of non printable chars
- * @types: Lista of arguments
+ * print_non_printable - Prints ASCII codes in hexa of non-printable characters
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * @flags: Calculates active flags
+ * @width: Get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: Number of characters printed
  */
 int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
@@ -74,11 +84,13 @@ int print_non_printable(va_list types, char buffer[],
 	UNUSED(precision);
 	UNUSED(size);
 
+	/* Check if the string is NULL */
 	if (str == NULL)
 		return (write(1, "(null)", 6));
 
 	while (str[i] != '\0')
 	{
+		/* Check if the character is printable or non-printable */
 		if (is_printable(str[i]))
 			buffer[i + offset] = str[i];
 		else
@@ -86,7 +98,7 @@ int print_non_printable(va_list types, char buffer[],
 
 		i++;
 	}
-
+	/* Set null terminator */
 	buffer[i + offset] = '\0';
 
 	return (write(1, buffer, i + offset));
@@ -95,17 +107,16 @@ int print_non_printable(va_list types, char buffer[],
 /************************* PRINT REVERSE *************************/
 /**
  * print_reverse - Prints reverse string.
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * @flags: Calculates active flags
+ * @width: Get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Numbers of chars printed
+ * Return: Number of characters printed
  */
-
 int print_reverse(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	char *str;
 	int i, count = 0;
@@ -115,17 +126,22 @@ int print_reverse(va_list types, char buffer[],
 	UNUSED(width);
 	UNUSED(size);
 
+	/* Get the string argument */
 	str = va_arg(types, char *);
 
+	/* Check if the string is NULL */
 	if (str == NULL)
 	{
 		UNUSED(precision);
 
 		str = ")Null(";
 	}
+
+	/* Calculate the length of the string */
 	for (i = 0; str[i]; i++)
 		;
 
+	/* Print the string in reverse */
 	for (i = i - 1; i >= 0; i--)
 	{
 		char z = str[i];
@@ -133,21 +149,23 @@ int print_reverse(va_list types, char buffer[],
 		write(1, &z, 1);
 		count++;
 	}
+
 	return (count);
 }
+
 /************************* PRINT A STRING IN ROT13 *************************/
 /**
- * print_rot13string - Print a string in rot13.
- * @types: Lista of arguments
+ * print_rot13string - Print a string in ROT13.
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
+ * @flags: Calculates active flags
+ * @width: Get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Numbers of chars printed
+ * Return: Number of characters printed
  */
 int print_rot13string(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	char x;
 	char *str;
@@ -157,6 +175,7 @@ int print_rot13string(va_list types, char buffer[],
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	str = va_arg(types, char *);
+
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
@@ -165,6 +184,7 @@ int print_rot13string(va_list types, char buffer[],
 
 	if (str == NULL)
 		str = "(AHYY)";
+
 	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; in[j]; j++)
@@ -184,5 +204,6 @@ int print_rot13string(va_list types, char buffer[],
 			count++;
 		}
 	}
+
 	return (count);
 }
