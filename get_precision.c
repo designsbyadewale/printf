95ebaffa3c6ac743fdef6d859e42c315bb1a7478
designsbyadewale <designsbyadewale@gmail.com>
2023-09-19 17:09:34 -0700

    update1

--- get_precision.c
+++ get_precision.c
@@ -1,48 +1,41 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Pointer to the current position in the format string.
- * @list: List of arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * Return: Precision value.
+ * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	/* Initialize the current index to the next character in the format string. */
 	int curr_i = *i + 1;
 	int precision = -1;
 
-	/* Check if the current character is not a dot (period). */
 	if (format[curr_i] != '.')
-		return (precision); /* If not, return -1 (no precision). */
+		return (precision);
 
-	precision = 0; /* Reset precision to 0 since a dot was found, */
-	/* indicating precision. */
+	precision = 0;
 
-	/* Loop through characters after the dot to calculate precision. */
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (isdigit(format[curr_i]))
+		if (is_digit(format[curr_i]))
 		{
-			/* If the current character is a digit, update precision accordingly. */
 			precision *= 10;
 			precision += format[curr_i] - '0';
 		}
 		else if (format[curr_i] == '*')
 		{
-			/* If an asterisk (*) is encountered, retrieve precision from arguments. */
 			curr_i++;
 			precision = va_arg(list, int);
-				break;
+			break;
 		}
 		else
-			break; /* Exit the loop if a non-digit or non-asterisk */
-		/* character is encountered. */
+			break;
 	}
 
 	*i = curr_i - 1;
+
 	return (precision);
 }
